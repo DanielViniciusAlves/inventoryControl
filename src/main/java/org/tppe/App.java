7d621831d4d1f124b9e5c6397811028cdd479ff9
DanielViniciusAlves <danielribeiroalves0@gmail.com>
2023-11-26 23:56:10 -0300

    Adding usage example in main.

--- src/main/java/org/tppe/App.java
+++ src/main/java/org/tppe/App.java
@@ -1,7 +1,22 @@
 package org.tppe;
 
+import org.tppe.entities.Product;
+import org.tppe.entities.Stock;
+import org.tppe.exceptions.BlankDescriptionException;
+import org.tppe.exceptions.InvalidValueException;
+import org.tppe.services.StockServices;
+
 public class App {
     public static void main(String[] args) {
-        System.out.println("App");
+        StockServices stockServices = new StockServices();
+        stockServices.receiveProduct("Test", "12345-bh1", 12.3, 15.3, 10);
+
+        Product product = stockServices.findProductByName("Test");
+        System.out.println(product.getName());
+        System.out.println(product.getBarcode());
+
+        product = stockServices.findProductByBarcode("12345-bh1");
+        System.out.println(product.getName());
+        System.out.println(product.getBarcode());
     }
 }
