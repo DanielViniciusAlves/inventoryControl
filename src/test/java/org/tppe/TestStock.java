ada725b48d473d8a013360945daa1a425e1b9305
Ugor Marcilio Brandão Costa <52638444+ubrando@users.noreply.github.com>
2023-11-30 16:01:32 -0300

    correção de erros

--- src/test/java/org/tppe/TestStock.java
+++ src/test/java/org/tppe/TestStock.java
@@ -1,6 +1,6 @@
 package org.tppe;
 
-import org.junit.Before;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.params.ParameterizedTest;
