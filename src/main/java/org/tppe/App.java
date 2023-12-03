af34068dc60730dff109e8c1743966fa918d2301
DanielViniciusAlves <danielribeiroalves0@gmail.com>
2023-12-03 10:39:20 -0300

    Adding main menu in App.

--- src/main/java/org/tppe/App.java
+++ src/main/java/org/tppe/App.java
@@ -4,28 +4,100 @@ import org.tppe.entities.Product;
 import org.tppe.exceptions.BlankDescriptionException;
 import org.tppe.exceptions.InvalidValueException;
 import org.tppe.services.StockServices;
+import java.util.Scanner;
 
 import java.time.LocalDate;
 
 public class App {
     public static void main(String[] args) {
         StockServices stockServices = new StockServices();
-        LocalDate date = LocalDate.parse("2023-11-27");
-        stockServices.receiveProduct("Test", "12345", 12.3, 15.3, 1, date);
-        System.out.println(stockServices.getBatchTotal(0));
-        System.out.println(stockServices.getTotalProductsByName("Test"));
+        Scanner scanner = new Scanner(System.in);
 
-        stockServices.receiveProduct("Test", "12345", 12.3, 15.3, 10, date);
-        System.out.println(stockServices.getBatchTotal(0));
-        System.out.println(stockServices.getBatchTotal(1));
-        System.out.println(stockServices.getTotalProductsByName("Test"));
+        int choice;
+        do {
+            System.out.println("Menu:");
+            System.out.println("1. Receive Product");
+            System.out.println("2. Find Product by Name");
+            System.out.println("3. Find Product by Barcode");
+            System.out.println("4. Return Product");
+            System.out.println("5. Get Total Products by Barcode");
+            System.out.println("6. Get Total Products by Name");
+            System.out.println("7. Sell Product");
+            System.out.println("8. Get Batch Total");
+            System.out.println("9. Check Low Stock Alert");
+            System.out.println("0. Exit");
+            System.out.print("Enter your choice: ");
 
-        Product product = stockServices.findProductByName("Test");
-        System.out.println(product.getName());
-        System.out.println(product.getBarcode());
+            choice = scanner.nextInt();
+            scanner.nextLine(); // Consume the newline character
 
-        product = stockServices.findProductByBarcode("12345");
-        System.out.println(product.getName());
-        System.out.println(product.getBarcode());
+            switch (choice) {
+                case 1:
+                    System.out.print("Enter product details (name, barcode, buyPrice, sellPrice, quantity, expirationDate): ");
+                    String[] productDetails = scanner.nextLine().split("\\s");
+                    stockServices.receiveProduct(productDetails[0], productDetails[1],
+                            Double.parseDouble(productDetails[2]), Double.parseDouble(productDetails[3]),
+                            Integer.parseInt(productDetails[4]), LocalDate.parse(productDetails[5]));
+                    break;
+                case 2:
+                    System.out.print("Enter product name: ");
+                    String name = scanner.nextLine();
+                    Product productNameInfo = stockServices.findProductByName(name);
+                    System.out.println("Information found: " + productNameInfo.getName() + productNameInfo.getQuantity());
+                    break;
+                case 3:
+                    System.out.print("Enter product barcode: ");
+                    String barcode = scanner.nextLine();
+                    Product productBarcodeInfo = stockServices.findProductByBarcode(barcode);
+                    System.out.println("Information found: " + productBarcodeInfo.getName() + productBarcodeInfo.getQuantity());
+                    break;
+                case 4:
+                    System.out.print("Enter product details for return (name, quantity): ");
+                    String[] returnDetails = scanner.nextLine().split("\\s");
+                    stockServices.returnProduct(returnDetails[0], Integer.parseInt(returnDetails[1]));
+                    break;
+                case 5:
+                    System.out.print("Enter product barcode: ");
+                    barcode = scanner.nextLine();
+                    int productBarcodeQnt = stockServices.getTotalProductsByBarcode(barcode);
+                    System.out.println("Quantity stored: " + productBarcodeQnt);
+                    break;
+                case 6:
+                    System.out.print("Enter product name: ");
+                    name = scanner.nextLine();
+                    int productNameQnt = stockServices.getTotalProductsByName(name);
+                    System.out.println("Quantity stored: " + productNameQnt);
+                    break;
+                case 7:
+                    System.out.print("Enter product details for sale (name, quantity): ");
+                    String[] saleDetails = scanner.nextLine().split("\\s");
+                    stockServices.sellProduct(saleDetails[0], Integer.parseInt(saleDetails[1]));
+                    break;
+                case 8:
+                    System.out.print("Enter batch ID: ");
+                    int batchId = scanner.nextInt();
+                    int batchTotal = stockServices.getBatchTotal(batchId);
+                    System.out.println("Batch Total: " + batchTotal);
+                    break;
+                case 9:
+                    System.out.print("Enter product details for low stock alert (name, minimum): ");
+                    String[] lowStockDetails = scanner.nextLine().split("\\s");
+                    boolean alert = stockServices.hasLowStockAlert(lowStockDetails[0], Integer.parseInt(lowStockDetails[1]));
+                    if (alert) {
+                        System.out.println("Low stock!");
+                    } else {
+                        System.out.println("Stock above minimum.");
+                    }
+                    break;
+                case 0:
+                    System.out.println("Exiting the program. Goodbye!");
+                    break;
+                default:
+                    System.out.println("Invalid choice. Please enter a valid option.");
+            }
+
+        } while (choice != 0);
+
+        scanner.close();
     }
 }
